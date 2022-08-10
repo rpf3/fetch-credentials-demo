852c5bd86ddc86ab2303514edf318a11c35317e0
Dickie Field <me@rpf3.io>
2022-08-10 15:19:52 -0400

    Initial commit

---
+++ src/routes/index.ts
@@ -0,0 +1,23 @@
+import type { RequestEvent } from "@sveltejs/kit";
+
+export async function GET(event: RequestEvent) {
+	const headers = event.request.headers;
+
+	if (headers.has('Authorization') === false) {
+		return {
+			status: 401
+		}
+	}
+
+	const bearer = headers.get('Authorization');
+
+	// verify bearer token
+
+	// make secure API calls
+
+  return {
+    body: {
+			name: 'rpf3'
+		}
+  };
+}
